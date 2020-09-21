5031f97c6de43258811a70f395a6e76bcea50829
사용자 <@LAPTOP-9IFJ2FT0>
2020-09-21 11:54:39 +0900

    2020.09.21 Project create

---
+++ FinalProject_fourMyung/src/main/java/com/example/demo/FinalProjectFourMyungApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectFourMyungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectFourMyungApplication.class, args);
+	}
+
+}
