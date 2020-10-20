02127c7839537585afeef9fd2959bc645960f028
사용자 <@LAPTOP-9IFJ2FT0>
2020-10-20 17:22:07 +0900

    2020.10.20 CYS

--- FinalProject_fourMyung/src/main/java/fourMyung/pay/service/PaymentInfogService.java
+++ FinalProject_fourMyung/src/main/java/fourMyung/pay/service/PaymentInfogService.java
@@ -14,15 +14,27 @@ public class PaymentInfogService {
 	
 	@Autowired
 	PayMapper payMapper;
-	
-	public void payInsert(PayCommand paycommand) throws Exception{
+	@Autowired
+	PaymentInfogService paymentInfogService;
+	public void payInsert(PayCommand paycommand, String userId) throws Exception{
 		// TODO Auto-generated method stub
 		PayDTO payDto = new PayDTO();
 		payDto.setPayType(paycommand.getPayType());
 		payDto.setPayPrice(paycommand.getPrice());
-		
+		payDto.setPayDetail("KP");
+		payDto.setCardNum("1234");
+		payDto.setUserId(userId);
+		payDto.setSttusDivCd("Y"); // Y:결제완료 C:결제취소
 		Integer i = payMapper.payInfoInsert(payDto);
 		System.out.println(i+"개의 결제 정보가 입력되었습니다.");
+		
 	}
-
+	public void payHotelInsert(String userId) throws Exception{
+		// TODO Auto-generated method stub
+		Integer j = payMapper.hotelPayInsert(userId);
+		System.out.println(j+"개의 호텔결제 정보가 입력되었습니다.");
+	}
+	public void payleisureInsert(String userId) {
+		
+	}
 }
